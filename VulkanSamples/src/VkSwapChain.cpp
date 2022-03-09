c0743f1d219cabc1abf02cf932cd0ad271ca2314
zzx <1181469308@qq.com>
2022-03-09 18:50:05 +0800

    添加宏函数 用来获取实例或者设备相关的扩展(WSI扩展API函数)[C

--- VulkanSamples/src/VkSwapChain.cpp
+++ VulkanSamples/src/VkSwapChain.cpp
@@ -1,4 +1,16 @@
 #include<VkSwapChain.h>
+#define INSTANCE_FUNC_PTR(instance,entrypoint){\
+fp##entrypoint=(PFN_vk_##entrypoint) vkGetInstanceProcAddr(intsance,"vk"#entrypoint);\
+if(fp##entrypoint==nullptr){\
+std::cout<<"Unable to locate the vkGetInstanceProcAddr: vk"#entrypoint<<std::endl;\
+	}\
+}
+#define DEVICE_FUBC_PTR(dev,entrypoint){\
+fp##entrrpoint=(PFN_vk##entrypoint) vkGetDeviceProcAddr(instance,"vk"#entrypoint);\
+if(fp##entrypoint==nullptr){\
+std::cout<<"Uable to locate the vkGetDeviceProcAddr: vk"#entrypoint<<std::endl;\
+}\
+}
 VulkanSwapChain::VulkanSwapChain()
 {
 }
