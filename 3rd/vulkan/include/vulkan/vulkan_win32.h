61fb5184342f92d33abd7b9264b59a97154df25a
zzx <1181469308@qq.com>
2022-03-29 16:48:28 +0800

    2022/3/29

--- 3rd/vulkan/include/vulkan/vulkan_win32.h
+++ 3rd/vulkan/include/vulkan/vulkan_win32.h
@@ -12,7 +12,6 @@
 **
 */
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
--- TestVK/main.cpp
+++ TestVK/main.cpp
@@ -1,26 +1,9 @@
 #include<vulkan/vulkan.h>
 
-std::vector<const char *> instanceExtensionNames = { VK_KHR_SURFACE_EXTENSION_NAME,
-													"VK_KHR_win32_surface",
-													 VK_EXT_DEBUG_REPORT_EXTENSION_NAME };
-std::vector<const char*> layerNames = { 
-										"VK_LAYER_LUNARG_api_dump",
-										//"VK_LAYER_KHRONOS_validation",								
-};
-std::vector<const char*> deviceExtensionNames = {
-	VK_KHR_SWAPCHAIN_EXTENSION_NAME
-};
-
-void createInstance()
-{
-	VkApplicationInfo appInfo = {};
-	VkInstanceCreateInfo createInfo = {};
-	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	createInfo.flags = 0;
-	createInfo.pNext = nullptr;
-	createInfo.pApplicationInfo=
-	vkCreateInstance( , );
-}
+/*
+* 此demo用来描述用vulkan绘制一个三角形
+* 数学库使用glm来实现
+*/
 
 int main(int argc,char **argv)
 {
